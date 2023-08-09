1be5d2fb876e2a85a85d3623e8b39d434b1899f3
andriy <evicandrij@gmail.com>
2023-08-09 19:47:05 +0300

    comit

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,6 +1,6 @@
 import {useState,useEffect} from 'react'
 import { useParams } from "react-router-dom";
-import axios from 'axios'
+import  axios from 'axios'
 // import css from './Cast.module.css'
 
 function Cast() {
