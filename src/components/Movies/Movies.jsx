99506361a773e26ce2df496ed62ad64cd539471a
andriy <evicandrij@gmail.com>
2023-11-13 14:00:10 +0200

    comit

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Notiflix from 'notiflix';
 import { Link, useLocation, useSearchParams } from 'react-router-dom';
-import { TailSpin } from 'react-loader-spinner';
+
 
 import css from './Movies.module.css';
 
